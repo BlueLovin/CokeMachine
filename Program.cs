585a704c3f3c74deb3566dd98d2b58ee15e09fc0
Matthue <matthewjury2@gmail.com>
2020-04-01 05:10:35 -0400

    Complete rewrite of program
    Less verbose, more object oriented code. User balance still in tact. User now has an inventory. Money is represented as decimal instead of a floating point number
    
    Co-Authored-By: Seth Centerbar <sethcenterbar@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,171 +1,44 @@
 ﻿using System;
-using System.Data;
-using CokeMachine;
 
-namespace CokeMachine
+namespace CSharpPlayground
 {
     class Program
     {
-        public static float userBalance, selectionPrice;
-        public static string selectionID;
         static void Main(string[] args)
         {
-            mainMenu(0f);
-        }
-        static void mainMenu(float balance)
-        {
-            
-            coke Coke = new coke();
-            dietCoke dietCoke = new dietCoke();
-            sprite Sprite = new sprite();
-            drPepper DrPepper = new drPepper();
-            dasani Dasani = new dasani();
-            DateTime date = DateTime.Now;
-            Console.Clear();
+            var vendingMachine = new vendingMachine();
+            var sodas = new Sodas();
+            var user = new User();
 
-            Console.WriteLine("Welcome to the Coke Machine.");
-            Console.WriteLine("The date is: {0: MM/dd/yyyy HH:mm:ss}.", date);
-            colorGreen();
-            Console.WriteLine("\nYour current balance: ${0}", balance);
-            colorWhite();
-
-            colorWhite();
-            Console.WriteLine("\nHere are your options:");
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("[1]Coca-Cola - ${0}", coke.price);
-
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("[2]Diet Coke - ${0}", dietCoke.price);
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("[3]Dr. Pepper - ${0}", drPepper.price);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("[4]Sprite - ${0}", sprite.price);
-
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("[5]Dasani Water - ${0}", dasani.price);
-            bool okay = false;
-            while (okay == false)
+            while (true)
             {
-                colorWhite();
-                Console.WriteLine();
-                Console.WriteLine("What will your selection be?");
-                string selectionStr = Console.ReadLine();
-                int selectionInt;
-                if (int.TryParse(selectionStr, out selectionInt) && selectionInt < 6 && selectionInt > 0) // if value entered is acceptable
+                Console.Clear();
+                vendingMachine.printHomeScreen();
+
+                int selection = vendingMachine.Selection();
+                if (User.Balance >= User.CurrentDrinkPrice)
                 {
-                    insertCash(selectionInt, balance);
-                    okay = true;
+                    vendingMachine.cashFlow(selection);
                 }
                 else
-                    Console.WriteLine("Invalid selection! Try again.");
-            }
-        }
-        public static void insertCash(int selection, float balance)
-        {
-            //
-            //Setting values according to the selection that was passed to this method.
-            //
-            if (selection == 1)
-            {
-                selectionID = "Coca-Cola";
-                selectionPrice = coke.price;
-            }
-
-            if (selection == 2)
-            {
-                selectionID = "Diet Coke";
-                selectionPrice = dietCoke.price;
-            }
-
-            if (selection == 3)
-            {
-                selectionID = "Dr. Pepper";
-                selectionPrice = drPepper.price;
-            }
-
-            if (selection == 4)
-            {
-                selectionID = "Sprite";
-                selectionPrice = sprite.price;
-            }
-
-            if (selection == 5)
-            { 
-                selectionID = "Dasani";
-                selectionPrice = dasani.price;
-            }
-            ///
-            ///
-            ///
-                if (selectionPrice > balance)
                 {
-                    Console.WriteLine("You chose {0}. Current balance: ${1}" +
-                        "\nPlease insert $" + (selectionPrice - balance), selectionID, balance);
+                    vendingMachine.insertCash(selection,
+                        sodas.DrinkPrices[selection],
+                        User.Balance);
                 }
-                if (balance >= selectionPrice)
+
+                Console.WriteLine("Would you like to buy another drink? [y/n]");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "y" || input.ToLower() == "y ")
                 {
-                    Console.WriteLine("Thank you. Enjoy your {0}", selectionID);
-                    balance -= selectionPrice;
-                    giveChange(balance);
+                    continue;
                 }
-            try
-            {
-                do
+                else
                 {
-                    balance += float.Parse(Console.ReadLine());
-                    Console.WriteLine("Current Balance: ${0}", (balance - selectionPrice).ToString("#.##"));
-                } while (balance < selectionPrice);
-            }
-            catch
-            {
-                Console.WriteLine("Invalid input. Try again.");
-                insertCash(selection, balance);
-            }
-                if (balance >= selectionPrice)
-                {
-                    float balanceLocal = balance - selectionPrice;
-                    giveChange(balanceLocal);
+                    break;
                 }
-        }
-        static void giveChange(float change)
-        {
-            Console.WriteLine("Would you like to buy another drink? [Y\\N]");
-            string input = Console.ReadLine();
-            if (input == "y")
-                mainMenu(change);
-            if (input == "n")
-            {
-                Console.WriteLine("Thanks. Your change is ${0}. Enjoy your {1}", change, selectionID);
-                //Console.ReadKey();
             }
-            else
-            {
-                Console.WriteLine("Invalid input.");
-                giveChange(change);
-            }
-            Console.ReadKey();
-            Environment.Exit(0); // EXITS AT THE END OF giveChange FUNCTION
-
-        }
-
-
-        /// <summary>
-        /// COLORS!!!
-        /// </summary>
-        static void colorWhite()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-        static void colorGreen()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-        }
-        static void colorRed()
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPlayground
+{
+    class User
+    {
+        public static decimal CurrentDrinkPrice { get; set; }
+        public static decimal Balance { get; set; }
+        public static List<string> Inventory = new List<String>(); //inventory
+    }
+    class Sodas
+    {
+        public static decimal selectionPrice { get; set; }
+        public IDictionary<int, string> DrinkList 
+            = new Dictionary<int, string>()
+        {
+            {0,"Coca-Cola" },
+            {1, "Sprite" },
+            {2, "Mtn. Dew" },
+            {3, "Dr. Pepper" }
+        };
+        public IDictionary<int, decimal> DrinkPrices 
+            = new Dictionary<int, decimal>()
+        {
+            {0, 1.25m },//coke
+            {1, 1.50m },//sprite
+            {2, 2.00m },//mtn dew
+            {3, 0.75m }//dr pepper
+        };
+    }
+    class vendingMachine
+    {
+        /// <summary>
+        /// greets user and prints all items in the Drink Dictionary
+        /// prices as well
+        /// </summary>
+        public static void printHomeScreen()
+        {
+            var sodas = new Sodas();
+            var user = new User();
+
+            Console.WriteLine("Welcome to the Coke Machine.\nYour balance is: $"
+                + User.Balance + "\n");
+
+            if (User.Inventory.Count > 0)
+            {
+                Console.Write("Items that you have bought: ");
+                foreach (string item in User.Inventory)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPlease choose from the following:");
+
+            //print all drinks in list
+            for (int i = 0; i < sodas.DrinkList.Count; i++) 
+            {
+                Console.WriteLine("[{0}] " + sodas.DrinkList[i] +
+                    " - ${1}", i, sodas.DrinkPrices[i]);
+            }
+        }
+
+        public int Selection()
+        {
+            int selection;
+            var sodas = new Sodas();
+            
+            while (true)
+            {
+                Console.Write("\nYour selection: ");
+                string userInput = Console.ReadLine();
+                int inputInt;
+                if (int.TryParse(userInput, out inputInt)
+                    && int.Parse(userInput) >= 0
+                    && int.Parse(userInput) < 4)//checks if input is valid
+                {
+                    selection = inputInt;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try Again.... Fucking douche.");
+                    continue;
+                }
+            }
+            User.CurrentDrinkPrice = sodas.DrinkPrices[selection];
+            Sodas.selectionPrice = sodas.DrinkPrices[selection];
+            return selection;
+        }
+                
+        public static void insertCash(int selection, decimal drinkPrice, decimal userBalance)
+        {
+            var sodas = new Sodas();
+            var vendingmachine = new vendingMachine();
+            //SETS PUBLIC PRICE VARIABLE
+            decimal selectionPrice = sodas.DrinkPrices[selection];
+            Console.WriteLine("\nYour selection was {0}",
+                sodas.DrinkList[selection]);
+            Console.WriteLine("Your current balance is ${0}",
+                User.Balance);
+
+            String drinkSelectionStr =
+                sodas.DrinkList[selection];
+            decimal inputDecimal;
+            if (User.Balance >= selectionPrice)
+            {
+                cashFlow(selection);
+            }
+            while (true) //loop checks if input is valid
+            {
+                Console.WriteLine("Please insert ${0}",
+                    (sodas.DrinkPrices[selection] - User.Balance));
+                Console.Write("\n$");
+                string inputString = Console.ReadLine();
+                if (decimal.TryParse(inputString, out inputDecimal))
+                    break;
+                else
+                {
+                    Console.WriteLine("Try again dickhead");
+                    continue;
+                }
+            }
+            User.Balance += inputDecimal;
+            cashFlow(selection);
+        }
+
+        public static void cashFlow(int drinkSelectionInt)
+        {
+            var user = new User();
+            var sodas = new Sodas();
+            var vendingmachine = new vendingMachine();
+            decimal drinkPrice = Sodas.selectionPrice;
+            decimal balance = User.Balance;
+            Console.Clear();
+            if (balance < drinkPrice) // ask for more cash!
+            {
+                insertCash(drinkSelectionInt, drinkPrice, User.Balance);
+            }
+
+            if (balance > drinkPrice) // give change
+            {
+                User.Balance -= drinkPrice;
+                decimal change = (balance - drinkPrice);
+                Console.WriteLine("Your change is ${0}." +
+                    "\nEnjoy your {1}", change, sodas.DrinkList[drinkSelectionInt]);
+                //back to main
+            }
+
+            if (balance == drinkPrice)
+            {
+                User.Balance -= drinkPrice;
+                Console.WriteLine("\nEnjoy your {0}",
+                    sodas.DrinkList[drinkSelectionInt]);
+                //back to main
+            }
+            User.Inventory.Add(sodas.DrinkList[drinkSelectionInt]);//appends newly bought soda to user inventory
+            //exit to main function
+        }
+    }
+}
